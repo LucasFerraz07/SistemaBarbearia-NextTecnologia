e822cf4f4281dfce87fddb07b6d6b5714b377a89
LucasFerraz07 <lucas.ferrazm0411@gmail.com>
2026-04-22 15:02:42 -0300

    chore: install and configure L5-Swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(
+    title: 'Sistema Barbearia API',
+    version: '1.0.0',
+    description: 'API RESTful para gerenciamento de barbearia',
+    contact: new OA\Contact(email: 'seuemail@email.com')
+)]
+#[OA\SecurityScheme(
+    securityScheme: 'bearerAuth',
+    type: 'http',
+    scheme: 'bearer',
+    bearerFormat: 'JWT'
+)]
 abstract class Controller
 {
     //
