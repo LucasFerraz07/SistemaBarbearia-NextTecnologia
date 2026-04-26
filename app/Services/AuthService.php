2fb582aca0ea5c74176c3b8f1f29b9ed3a2b1964
LucasFerraz07 <lucas.ferrazm0411@gmail.com>
2026-04-26 12:52:45 -0300

    fix: wrap client registration in database transaction

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -7,6 +7,7 @@ use App\Models\City;
 use App\Models\Client;
 use App\Models\User;
 use App\Models\UserType;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
 class AuthService
@@ -23,28 +24,32 @@ class AuthService
 
         $clientType = UserType::where('name', 'cliente')->firstOrFail();
 
-        $user = User::create([
-            'name'         => $data['name'],
-            'email'        => $data['email'],
-            'password'     => $data['password'],
-            'user_type_id' => $clientType->id,
-        ]);
+        $user = DB::transaction(function () use ($data, $cepData, $clientType) {
+            $user = User::create([
+                'name'         => $data['name'],
+                'email'        => $data['email'],
+                'password'     => $data['password'],
+                'user_type_id' => $clientType->id,
+            ]);
 
-        $city = City::firstOrCreate(['name' => $cepData['localidade']]);
+            $city = City::firstOrCreate(['name' => $cepData['localidade']]);
 
-        $address = Address::create([
-            'city_id'      => $city->id,
-            'street'       => $cepData['logradouro'],
-            'number'       => $data['number'],
-            'neighborhood' => $cepData['bairro'],
-            'cep'          => $data['cep'],
-        ]);
+            $address = Address::create([
+                'city_id'      => $city->id,
+                'street'       => $cepData['logradouro'],
+                'number'       => $data['number'],
+                'neighborhood' => $cepData['bairro'],
+                'cep'          => $data['cep'],
+            ]);
 
-        Client::create([
-            'user_id'    => $user->id,
-            'phone'      => $data['phone'],
-            'address_id' => $address->id,
-        ]);
+            Client::create([
+                'user_id'    => $user->id,
+                'phone'      => $data['phone'],
+                'address_id' => $address->id,
+            ]);
+
+            return $user;
+        });
 
         return $user->load('client.address.city');
     }
